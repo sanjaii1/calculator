db7da40465569035ba15a081ae5dd0d0bd7c2606
sanjaii1 <bsanjay235@gmail.com>
2025-03-13 00:10:22 +0530

    start design

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-     <h1>Set up Vite</h1>
+      <div className="calculator">
+        <div className="container">
+          <div className="card">
+           
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
